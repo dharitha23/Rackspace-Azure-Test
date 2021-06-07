d55a56fcc25890e6594b36369d3a2143a1fe04a6
dharitha23 <harithadesikan@outlook.com>
2021-06-07 06:22:24 -0400

    edit edit edit

--- scripts/vmssscript.sh
+++ scripts/vmssscript.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-dpkg --configure -a
-apt-get -y update
+sudo apt-get -y update
 
 # install Apache2
-apt-get -y install apache2
+sudo apt-get -y install apache2
 
 # write some HTML
-echo \<center\>\<h1\>Hello World from $Hostname\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+echo \<center\>\<h1\>Hello World from $Hostname\</h1\>\<br/\>\</center\> > /var/www/Helloworld.html
 
 # restart Apache
-apachectl restart
+sudo systemctl restart apache2.service
